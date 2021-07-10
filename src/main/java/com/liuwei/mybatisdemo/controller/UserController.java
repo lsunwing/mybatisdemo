f0290c5c0e4f38bd9a182e3ade9a99fc70f192a3
William S Lewis <31364812+lsunwing@users.noreply.github.com>
2021-07-11 00:26:24 +0800

    add log4j2

--- src/main/java/com/liuwei/mybatisdemo/controller/UserController.java
+++ src/main/java/com/liuwei/mybatisdemo/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.liuwei.mybatisdemo.dao.entity.User;
 import com.liuwei.mybatisdemo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserService userService;
 
     @RequestMapping("/queryAllUsers")
     @ResponseBody
     public List<User> queryAllUsers() {
+        logger.info("queryAllUsers begin.");
+
         return userService.queryAllUsers();
     }
 }
