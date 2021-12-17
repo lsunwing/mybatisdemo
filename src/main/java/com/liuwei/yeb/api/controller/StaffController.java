8c7b7d05338d2bc46204d1449d9c9f1083d0eb37
William S Lewis <31364812+lsunwing@users.noreply.github.com>
2021-12-18 00:58:11 +0800

    2021/12/17

--- src/main/java/com/liuwei/yeb/api/controller/StaffController.java
+++ src/main/java/com/liuwei/yeb/api/controller/StaffController.java
@@ -1,18 +1,17 @@
 package com.liuwei.yeb.api.controller;
 
 
-import java.util.List;
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.liuwei.yeb.api.common.DataResponse;
 import com.liuwei.yeb.api.entity.Staff;
 import com.liuwei.yeb.api.service.impl.StaffServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -38,13 +37,36 @@ public class StaffController {
     public DataResponse<String> addStaff(@RequestBody Staff staff) {
         System.out.println(staff);
         staffService.saveOrUpdate(staff);
-        return new DataResponse("0000", "success");
+        return new DataResponse("0000", "add success");
     }
 
-    @RequestMapping("/list")
+    @RequestMapping(value = "/modify", method = RequestMethod.POST)
     @ResponseBody
-    public DataResponse list() {
-        List<Staff> list = staffService.list();
-        return new DataResponse("0000", "success", list);
+    public DataResponse<String> modifyStaff(@RequestBody Staff staff) {
+        System.out.println(staff);
+        staffService.saveOrUpdate(staff);
+        return new DataResponse("0000", "modify success");
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.DELETE)
+    @ResponseBody
+    public DataResponse<String> removeStaff(Long staffId) {
+        System.out.println(staffId);
+        staffService.removeById(staffId);
+        return new DataResponse("0000", "remove success");
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    @ResponseBody
+    public DataResponse list(@RequestBody Staff staff) {
+        System.out.println(staff);
+        QueryWrapper<Staff> wrapper = new QueryWrapper<>();
+        if (StringUtils.hasText(staff.getStaffName())) {
+            wrapper.eq("STAFF_NAME", staff.getStaffName());
+        }
+        if (StringUtils.hasText(staff.getGender())) {
+            wrapper.eq("GENDER", staff.getGender());
+        }
+        return new DataResponse("0000", "list success", staffService.list(wrapper));
     }
 }
